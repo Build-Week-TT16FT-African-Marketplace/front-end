98e6e569d9b0e1328589f5dd223db3935c658070
Steven Sussman <sjsussman@gmail.com>
2020-10-19 13:33:49 -0400

    set up general / private routing

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -3,6 +3,9 @@ import {Route, Redirect } from 'react-router-dom'
 import './App.css';
 
 import Landing from './components/Landing'
+import PrivateRoute from './components/PrivateRoute'
+import MainMarketPage from './components/MainMarketPage'
+import Login from './components/Login'
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Redirect to="/landing" />
       </Route>
       <Route path='/landing' component={Landing} />
+      <Route path ='/login' component={Login} />
+      <Route path ='/signup' component={Signup} />
+      <PrivateRoute exact path="/market" component={MainMarketPage} />
     </div>
   );
 }
