4d97fdbda3272f0337e019dd7231b44558cd1622
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-19 14:36:53 -0500

    Setting up Login component.

---
+++ african-marketplace/src/components/Login.js
@@ -0,0 +1,18 @@
+//Import Statements
+import react from "react";
+//Styled components?
+
+
+//Initial form values, initial disabled should probably be written in App.js
+
+
+//Component Function
+export default function Login(){
+    //Pass Login(props), then destructure props here
+
+    //Temporary onSubmit, write this and onChange in App.js
+    
+   
+    //Build out JSX
+    return null;
+};
