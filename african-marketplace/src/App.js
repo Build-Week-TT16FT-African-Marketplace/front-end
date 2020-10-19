ed8a84dfaa442e2f0a5057df9a267243401b3321
Steven Sussman <sjsussman@gmail.com>
2020-10-19 15:50:22 -0400

    fixed routing on app

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,38 +1,23 @@
-import React from 'react';
-import {Route, Redirect, Link, Switch } from 'react-router-dom';
-import './App.css';
-import loginSchema from './validation/loginSchema';
-import signupSchema from './validation/signupSchema';
-import Signup from './components/Signup';
-import Login from './components/Login';
-
 import Landing from './components/Landing'
+import PrivateRoute from './components/PrivateRoute'
+import MainMarketPage from './components/MainMarketPage'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
   return (
     
     <div className="App">
-      <nav>
-        <Link to='/signup'>Sign Up</Link>
-        <Link to='/login'>Log In</Link>
-        <Link to='/landing'>Home</Link>
-      </nav>
-      <div className='routes'>
-      <Switch>
-        <Route path={'/login'}>
-          <Login />
-        </Route>
-        <Route path={'/signup'}>
-          <Signup />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/landing" />
-        </Route>
-        <Route path='/landing' component={Landing} />
-      </Switch>
-      </div>
+      <Route exact path="/">
+        <Redirect to="/landing" />
+      </Route>
+      <Route path='/landing' component={Landing} />
+      <Route path ='/login' component={Login} />
+      <Route path ='/signup' component={Signup} />
+      <PrivateRoute exact path="/market" component={MainMarketPage} />
     </div>
   );
 }
 
 export default App;
+
