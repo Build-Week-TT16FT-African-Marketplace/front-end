9ca43d74e8a6dc61b577d2a2bb1826a712e0fcf6
Steven Sussman <sjsussman@gmail.com>
2020-10-20 09:07:48 -0400

    added backend base url

--- african-marketplace/src/utils/axiosWithAuth.js
+++ african-marketplace/src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosWithAuth = () => {
   const token = window.localStorage.getItem('token');
   return axios.create({
-    baseURL: "<waiting for base url from Jenn",
+    baseURL: "https://african-marketplace-back-end.herokuapp.com/",
     headers: {
     Authorization: token
     }  
