dd002a62b9f309c3c82ec89c9426f4be6f03656b
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-20 14:36:06 -0500

    Restarting branch.

---
+++ african-marketplace/src/components/SearchForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const testProduce = [
+    {
+        id: 1,
+        name: "corn",
+        description: "an ear of corn",
+        price: "$.46",
+        location: "Nairobi",
+        category: "vegetable",
+        URL: "URL",
+        user_id: "userId"
+    },
+    {
+        id: 2,
+        name: "eggplant",
+        description: "an eggplant",
+        price: "$.75",
+        location: "Nairobi",
+        category: "vegetable",
+        URL: "URL",
+        user_id: "userId"
+    },
+    {
+        id: 3,
+        name: "carrot",
+        description: "a carrot",
+        price: "$.50",
+        location: "Mogadishu",
+        category: "vegetable",
+        URL: "URL",
+        user_id: "userId"
+    }
+];
+
+function SearchForm () {
+
+
+
+
+    return null;
+}
+
+export default SearchForm;
