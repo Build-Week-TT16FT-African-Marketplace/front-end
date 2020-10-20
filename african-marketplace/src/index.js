283de4275570a9a39ce34e4ca63215bbe6838742
Steven Sussman <sjsussman@gmail.com>
2020-10-20 19:04:23 -0400

    added redux store to app

--- african-marketplace/src/index.js
+++ african-marketplace/src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import { reducer } from "./store/reducers/userReducer";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { logger } from "redux-logger";
+import thunk from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
