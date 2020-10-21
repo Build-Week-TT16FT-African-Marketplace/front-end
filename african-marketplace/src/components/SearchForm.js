130488f70bad7fb107da19daab78275170c8e488
Dionne Stratton <mylife33@hotmail.com>
2020-10-21 13:31:20 -0500

    connected update item?

--- african-marketplace/src/components/SearchForm.js
+++ african-marketplace/src/components/SearchForm.js
@@ -1,71 +1,70 @@
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import Button from "@material-ui/core/Button";
+// import React from "react";
+// import TextField from "@material-ui/core/TextField";
+// import Autocomplete from "@material-ui/lab/Autocomplete";
+// import Button from "@material-ui/core/Button";
 
+//  function SearchForm () {
 
- function SearchForm () {
+//      const onSubmit = (event) => {
+//         event.preventDefault();
+//     }
 
-     const onSubmit = (event) => {
-        event.preventDefault();
-    }
+//     return(
+//         <div style={{ width: 300}}>
+//         <form onSubmit={onSubmit}>
 
-    return(
-        <div style={{ width: 300}}>
-        <form onSubmit={onSubmit}>
-            
-            <Autocomplete 
-                freeSolo="true"
-                id="autocomplete"
-                autoSelect="true"
-                // replace testProduce
-                options={testProduce.filter((option) => option.name)}
-                renderInput={params => (
-                    <TextField {...params} 
-                    label="Search" 
-                    margin="normal" 
-                    variant="outlined"
-                    InputProps={{ ...params.InputProps, type: "search"}}
-                    />
-                )}
-            />
-            <Button variant="contained" type="submit">Search</Button>
-        </form>
-    </div>
-    )
-};
+//             <Autocomplete
+//                 freeSolo="true"
+//                 id="autocomplete"
+//                 autoSelect="true"
+//                 // replace testProduce
+//                 options={testProduce.filter((option) => option.name)}
+//                 renderInput={params => (
+//                     <TextField {...params}
+//                     label="Search"
+//                     margin="normal"
+//                     variant="outlined"
+//                     InputProps={{ ...params.InputProps, type: "search"}}
+//                     />
+//                 )}
+//             />
+//             <Button variant="contained" type="submit">Search</Button>
+//         </form>
+//     </div>
+//     )
+// };
 
-export default SearchForm;
-//remove test data later
-const testProduce = [
-    {
-        id: 1,
-        name: "corn",
-        description: "an ear of corn",
-        price: "$.46",
-        location: "Nairobi",
-        category: "vegetable",
-        URL: "URL",
-        user_id: "userId"
-    },
-    {
-        id: 2,
-        name: "eggplant",
-        description: "an eggplant",
-        price: "$.75",
-        location: "Nairobi",
-        category: "vegetable",
-        URL: "URL",
-        user_id: "userId"
-    },
-    {
-        id: 3,
-        name: "carrot",
-        description: "a carrot",
-        price: "$.50",
-        location: "Mogadishu",
-        category: "vegetable",
-        URL: "URL",
-        user_id: "userId"
-    }
-];
+// export default SearchForm;
+// //remove test data later
+// const testProduce = [
+//     {
+//         id: 1,
+//         name: "corn",
+//         description: "an ear of corn",
+//         price: "$.46",
+//         location: "Nairobi",
+//         category: "vegetable",
+//         URL: "URL",
+//         user_id: "userId"
+//     },
+//     {
+//         id: 2,
+//         name: "eggplant",
+//         description: "an eggplant",
+//         price: "$.75",
+//         location: "Nairobi",
+//         category: "vegetable",
+//         URL: "URL",
+//         user_id: "userId"
+//     },
+//     {
+//         id: 3,
+//         name: "carrot",
+//         description: "a carrot",
+//         price: "$.50",
+//         location: "Mogadishu",
+//         category: "vegetable",
+//         URL: "URL",
+//         user_id: "userId"
+//     }
+// ];
--- african-marketplace/src/components/UpdateItem.js
+++ african-marketplace/src/components/UpdateItem.js
@@ -1,68 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
-import NewItem from './NewItem'
+import NewItem from "./NewItem";
+import { connect } from "react-redux";
+import { updateProduct } from "../store/actions";
 
 // import { makeStyles } from "@material-ui/core/styles";
 
 const initFormVals = {
-	name: '',
-	description: '',
-	price: '',
-	location: '',
-	category: '',
-}
+  name: "",
+  description: "",
+  price: "",
+  location: "",
+  category: "",
+};
 
 const UpdateItem = (props) => {
-	const [open, setOpen] = useState(false);
-	const [modalStyle] = useState(getModalStyle);
-	const [itemData, setItemData] = useState(initFormVals);
-	const [itemId, setItemId] = useState(null);
-	
+  const { updateProduct, product } = props;
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+  const [itemData, setItemData] = useState(initFormVals);
+  const [itemId, setItemId] = useState(null);
 
-	
-	const handleOpen = (e) => {
-		const title = props.data.name;
-		const description = props.data.description;
-		const price = props.data.price;
-		const location = props.data.location;
-		const category = props.data.category;
+  const handleOpen = (e) => {
+    const title = props.data.name;
+    const description = props.data.description;
+    const price = props.data.price;
+    const location = props.data.location;
+    const category = props.data.category;
 
-	
-		setItemData({ 
-			item_title: title, 
-			item_description: description, 
-			item_price: price, 
-			item_location: location, 
-			item_category: category, 
-		});
-		setItemId(itemId);
-		setOpen(true);
-	  };
-	
-	  const handleClose = () => {
-		setOpen(false);
-	  };
-	
-	  return (
-		<>
-		  <button onClick={handleOpen}>Edit</button>
-		  <Modal open={open} onClose={handleClose}>
-			<div style={modalStyle}>
-			  <NewItem itemData={itemData} itemId={itemId} />
-			</div>
-		  </Modal>
-		</>
-	  );
-}
+    setItemData({
+      item_title: title,
+      item_description: description,
+      item_price: price,
+      item_location: location,
+      item_category: category,
+    });
+    setItemId(itemId);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <button onClick={handleOpen}>Edit</button>
+      <Modal open={open} onClose={handleClose}>
+        <div style={modalStyle}>
+          <NewItem itemData={itemData} itemId={itemId} />
+        </div>
+      </Modal>
+    </>
+  );
+};
 
 function getModalStyle() {
-	const top = 50 + Math.round(Math.random() * 20) - 10;
-	const left = 50 + Math.round(Math.random() * 20) - 10;
-	return {
-	  top: `${top}%`,
-	  left: `${left}%`,
-	  transform: `translate(-${top}%, -${left}%)`,
-	};
-  }
+  const top = 50 + Math.round(Math.random() * 20) - 10;
+  const left = 50 + Math.round(Math.random() * 20) - 10;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
-export default UpdateItem;
+const mapStateToProps = (state) => {
+  return {
+    forSale: state.fetchForSale.forSale,
+    product: state.fetchForSale.product,
+  };
+};
+
+export default connect(mapStateToProps, { updateProduct })(UpdateItem);
