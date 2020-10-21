eb06cbad2d96aa3c7b200dfe08c04c01bb8c61f3
Steven Sussman <sjsussman@gmail.com>
2020-10-21 08:20:54 -0400

    fixed store

--- african-marketplace/src/index.js
+++ african-marketplace/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import reducer from "./ReduxStore/reducers/index"
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
