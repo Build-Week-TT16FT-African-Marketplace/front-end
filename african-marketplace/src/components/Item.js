5d43c822e0f15f487f7b0ca8e255c1bd708a93f7
s-munro <68410363+s-munro@users.noreply.github.com>
2020-10-20 20:42:58 -0400

    item card, map, and axioswithauth

--- african-marketplace/src/components/Item.js
+++ african-marketplace/src/components/Item.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import axios from 'axios';
-import styles from './components-styles/Item.css';
+// import styles from './components-styles/Item.css';
 
 const Item = (props) => {
 
-const { id, name, description, price, location, category, URL, user_id } = props;
+const { name, description, price, location } = props;
+
+console.log('name', name);
 
 return (
   <div className='item-container'>
     <div className='item'>
-      <h3>{props.data.name}</h3>
-      <h4>{props.data.category}</h4>
-      <p>{props.data.price}</p>
-      <h4>${props.data.description}</h4>
-      <h4>${props.data.location}</h4>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <h4>${price}</h4>
+      <h4>${location}</h4>
     </div>
   </div>
 
--- african-marketplace/src/components/MainMarketPage.js
+++ african-marketplace/src/components/MainMarketPage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import axios from 'axios';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import Item from './Item';
+
+const initialItems = []
+
 
 const MainMarketPage = () => {
 
+    const [ items, setItems ] = useState(initialItems)
+
+    const getItems = () => {
+        axiosWithAuth()
+            .get('https://african-marketplace-back-end.herokuapp.com/items')
+            .then((res) => {
+                items.push(res.data[0])
+                console.log(items);
+            })
+            .catch((err) => {
+                console.log('Error found: ', err)
+            });
+    };
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
 return (
-    <h1>Welcome! Search products here:</h1>
+    <div>
+        <h1>Welcome! Search products here:</h1>
+
+        <div>
+            {items.map((content, index) => {
+                console.log('map', content, index)
+                return <Item key={index} content={content} />
+            })}
+        </div>
+        {/* <Items data={} />*/}
+    </div>
 )
 }
 
