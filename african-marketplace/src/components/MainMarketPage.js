7672073e26cbb541076d8e7a5c1bae11f19d4df8
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-22 12:01:59 -0500

    Added headers and descriptions for main page.

--- african-marketplace/src/components/MainMarketPage.js
+++ african-marketplace/src/components/MainMarketPage.js
@@ -24,25 +24,29 @@ const [ items, setItems ] = useState([])
 
 return (
     <div>
-        <h1>Welcome! Browse products here:</h1>
+        <h1>SAUTI EAST AFRICA MARKETPLACE</h1>
+        <h2>List:</h2>
+            <p>Have an item you'd like to sell? Fill out the form below to create the listing!</p>
         <NewItem />
+        <h2>Browse:</h2>
+            <p>Take a look at all the listings our sellers have created!</p>
             {items.map((content) => {
                 // console.log('map', content.id, index);
                 return (
-                <Item 
-                        key={content.id} 
-                        name={content.name}
-                        description={content.description} 
+                    <Item 
+                    key={content.id} 
+                    name={content.name}
+                    description={content.description} 
                         price={content.price} 
                         location={content.location} 
                         category={content.category}  
                         cid={content.id}
                         />
                         )
-            })}
+                    })}
+        <h3>Search:</h3>
+            <p>Don't see what you're looking for? Use the search bar below to track down exactly what you need!</p>
         <SearchForm items={items}/>
-
-
     </div>
 )
 }
